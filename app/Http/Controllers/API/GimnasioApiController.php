647c8267cb14fc0db6ddcc4537ab82aa4438c75a
MolinaKevin <molinakevinalejandro@gmail.com>
2018-05-25 06:16:58 -0300

    Diff fo humans en gimnasio api

--- app/Http/Controllers/API/GimnasioApiController.php
+++ app/Http/Controllers/API/GimnasioApiController.php
@@ -79,7 +79,8 @@ class GimnasioAPIController extends AppBaseController
         foreach ($input as $item) {
             $asistencia = [];
             $asistencia['user_id'] = $item['credencial'];
-            $asistencia['horario'] = Carbon::createFromFormat('Y-m-d H:m:s', $item['horario'])->format('Y-m-d H:m:s');
+            $temp = Carbon::parse($item['horario'])->diffForHumans();
+            $asistencia['horario'] = Carbon::createFromFormat('Y-m-d H:m:s', $temp)->format('Y-m-d H:m:s');
             $this->asistenciaRepository->create($asistencia);
         }
 
