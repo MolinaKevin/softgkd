b9a8f97b418ef137783174bfda22cee9aab0a8ed
MolinaKevin <molinakevinalejandro@gmail.com>
2018-06-03 19:42:17 -0300

    Acomodados iconos del menu lateral

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -3,11 +3,11 @@
 </li>
 
 <li class="{{ Request::is('plans*') ? 'active' : '' }}">
-    <a href="{!! route('plans.index') !!}"><i class="fa fa-dollar"></i><span>Planes</span></a>
+    <a href="{!! route('plans.index') !!}"><i class="fa fa-folder-open"></i><span>Planes</span></a>
 </li>
 
 <li class="{{ Request::is('especials*') ? 'active' : '' }}">
-    <a href="{!! route('especials.index') !!}"><i class="fa fa-edit"></i><span>Planes especiales</span></a>
+    <a href="{!! route('especials.index') !!}"><i class="fa fa-calendar"></i><span>Planes especiales</span></a>
 </li>
 
 <li class="{{ Request::is('revisacions*') ? 'active' : '' }}">
@@ -19,7 +19,7 @@
 </li>
 
 <li class="{{ Request::is('pagos*') ? 'active' : '' }}">
-    <a href="{!! route('pagos.index') !!}"><i class="fa fa-credit-card"></i><span>Pagos</span></a>
+    <a href="{!! route('pagos.index') !!}"><i class="fa fa-usd"></i><span>Pagos</span></a>
 </li>
 
 <li class="treeview {{ Request::is('users*') ? 'active' : '' }}">
@@ -42,18 +42,13 @@
 
 
 <li class="{{ Request::is('roles*') ? 'active' : '' }}">
-    <a href="{!! route('roles.index') !!}"><i class="fa fa-edit"></i><span>Roles</span></a>
+    <a href="{!! route('roles.index') !!}"><i class="fa fa-sistemap"></i><span>Roles</span></a>
 </li>
 
 <li class="{{ Request::is('horarios*') ? 'active' : '' }}">
-    <a href="{!! route('horarios.index') !!}"><i class="fa fa-edit"></i><span>Horarios</span></a>
+    <a href="{!! route('horarios.index') !!}"><i class="fa fa-clock-o"></i><span>Horarios</span></a>
 </li>
 
 <li class="{{ Request::is('asistencias*') ? 'active' : '' }}">
-    <a href="{!! route('asistencias.index') !!}"><i class="fa fa-edit"></i><span>Asistencias</span></a>
+    <a href="{!! route('asistencias.index') !!}"><i class="fa fa-list-alt"></i><span>Asistencias</span></a>
 </li>
-
-<li class="{{ Request::is('huellas*') ? 'active' : '' }}">
-    <a href="{!! route('huellas.index') !!}"><i class="fa fa-edit"></i><span>Huellas</span></a>
-</li>
-
