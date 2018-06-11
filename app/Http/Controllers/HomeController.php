b78a02a62cb665cc419daa3ee7d86d7f7111e6d2
MolinaKevin <molinakevinalejandro@gmail.com>
2018-06-11 00:54:16 -0300

    Agregando dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Asistencia;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $ingresos = Asistencia::latest()
+            ->take(15)
+            ->get();
+        return view('home', compact('ingresos'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
+    <section class="content-header">
+        <h1>
+            GKD Administracion
+            <small>Version 1.0</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Dashboard</li>
+        </ol>
+    </section>
+    <section class="content">
 
+        <div class="col-md-8">
+            <!-- TABLE: LATEST ORDERS -->
+            <div class="box box-info">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Ultimos ingresos</h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i
+                                    class="fa fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body" style="">
+                    <div class="table-responsive">
+                        <table class="table no-margin">
+                            <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Estado</th>
+                                <th>Revisacion medica</th>
+                                <th>Dispositivo</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($ingresos as $ingreso)
+                                <tr>
+                                    <td>{{ $ingreso->nombre }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.table-responsive -->
+                </div>
+            </div>
+            <!-- /.box -->
         </div>
-    </div>
+    </section>
 @endsection
