5a326b505ef0119062420db68ed34f06d4772733
MolinaKevin <molinakevinalejandro@gmail.com>
2018-06-12 17:02:23 -0300

    Cambio en grupos de multiselect a checboxes

--- resources/views/familias/fields.blade.php
+++ resources/views/familias/fields.blade.php
@@ -4,11 +4,33 @@
     {!! Form::text('name', null, ['class' => 'form-control']) !!}
 </div>
 
-<!-- Usuarios Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('users', 'Usuarios:') !!}
-    <span id="helpBlock" class="help-block">Utilice ctrl. para seleccionar mas de una persona.</span>
-    {!! Form::select('users[]', \App\Models\User::orderBy('first_name')->get()->pluck('name', 'id'), null, ['id' => 'users', 'multiple' => 'multiple', 'class' => 'form-control']) !!}
+<!-- Horarios Field -->
+<div class="form-group col-sm-12">
+
+    <h3 class="box-title">{!! Form::label('users', 'Usuarios:') !!}</h3>
+    @php
+        $temp = 0;
+    @endphp
+    <div class="row">
+        @foreach(\App\Models\User::orderBy('first_name')->get()->pluck('name', 'id') as $key => $user)
+            @if (($temp%3) == 0 && $temp > 0)
+    </div>
+    <div class="row">
+        @endif
+        <div class="col-lg-4">
+            <div class="input-group">
+                <span class="input-group-addon">
+                    {!! Form::checkbox("users[]", $key, null, ['id' => "user-{$key}"]) !!}
+                </span>
+                {!! Form::label("user-{$key}", $user, ['class' => 'form-control']) !!}
+            </div>
+        </div>
+        @php
+            $temp++;
+        @endphp
+        @endforeach
+    </div>
+
 </div>
 
 <!-- Submit Field -->
