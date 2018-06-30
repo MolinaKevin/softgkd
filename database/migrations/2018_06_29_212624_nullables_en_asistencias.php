c4e5df27b507ea120303567563268af97b2d83db
MolinaKevin <molinakevinalejandro@gmail.com>
2018-06-29 21:28:45 -0300

    Agregada migracion nullable en asistencia

---
+++ database/migrations/2018_06_29_212624_nullables_en_asistencias.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class NullablesEnAsistencias extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('asistencias', function (Blueprint $table) {
+            $table->dropForeign(['dispositivo_id']);
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('dispositivo_id');
+            $table->integer('dispositivo_id')->unsigned()->nullable();
+            $table->dropColumn('user_id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('dispositivo_id')->references('id')->on('dispositivos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('asistencias', function (Blueprint $table) {
+            $table->dropForeign(['dispositivo_id']);
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('dispositivo_id');
+            $table->integer('dispositivo_id')->unsigned();
+            $table->dropColumn('user_id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('dispositivo_id')->references('id')->on('dispositivos')->onDelete('cascade');
+        });
+    }
+}
