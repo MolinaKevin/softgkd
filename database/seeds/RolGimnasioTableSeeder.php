019a4441a557f0ba36470bd497d36c552131bf3f
MolinaKevin <molinakevinalejandro@gmail.com>
2018-09-11 09:39:32 -0300

    Prueba seeder

--- database/seeds/RolGimnasioTableSeeder.php
+++ database/seeds/RolGimnasioTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Plan;
 use App\Models\PlanUser;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -15,7 +16,31 @@ class RolGimnasioTableSeeder extends Seeder
     {
         $users = User::all();
         foreach ($users as $user) {
-            $plan = $user->plans->find(34);
+
+            $plan = Plan::find(1);
+            $user->plans()->attach(1);
+            switch ($plan->date) {
+                case 0:
+                    $user->plans()->updateExistingPivot($plan->id, ['clases' => $plan->cantidad + $input['adicion']]);
+                    break;
+                case 1:
+                    $user->plans()->updateExistingPivot($plan->id, ['vencimiento' => Carbon::now()->addDays($plan->cantidad + $input['adicion'])->endOfDay()]);
+                    break;
+                case 2:
+                    $user->plans()->updateExistingPivot($plan->id, ['vencimiento' => Carbon::now()->addWeek()]);
+                    break;
+                case 3:
+                    $user->plans()->updateExistingPivot($plan->id, ['vencimiento' => Carbon::now()->addMonth()]);
+                    break;
+                case 4:
+                    $user->plans()->updateExistingPivot($plan->id, ['vencimiento' => Carbon::now()->addYear()]);
+                    break;
+                default:
+                    $user->plans()->updateExistingPivot($plan->id, ['clases' => $plan->cantidad + $input['adicion']]);
+                    break;
+            }
+            $user->save();
+            $plan = $user->plans->find(1);
             $pivot = PlanUser::find($plan->pivot->id);
             $pivot->adeudar();
         }
