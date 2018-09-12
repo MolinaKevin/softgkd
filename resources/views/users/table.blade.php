9728d1ae9d27da442e3b0575c1f20e8649a62ae5
MolinaKevin <molinakevinalejandro@gmail.com>
2018-09-12 14:47:40 -0300

    Quitado vista user show

--- resources/views/users/table.blade.php
+++ resources/views/users/table.blade.php
@@ -27,8 +27,8 @@
                     <a href="#" class='btn btn-default btn-xs btnTag'><i class="glyphicon glyphicon-tags"></i></a>
                     <a href="{!! route('users.plans', [$user->id]) !!}" class='btn btn-default btn-xs'><i
                                 class="glyphicon glyphicon-list-alt"></i></a>
-                    <a href="{!! route('users.show', [$user->id]) !!}" class='btn btn-default btn-xs'><i
-                                class="glyphicon glyphicon-eye-open"></i></a>
+                    <!--a href="{!! route('users.show', [$user->id]) !!}" class='btn btn-default btn-xs'><i
+                                class="glyphicon glyphicon-eye-open"></i></a-->
                     <a href="{!! route('users.edit', [$user->id]) !!}" class='btn btn-default btn-xs'><i
                                 class="glyphicon glyphicon-edit"></i></a>
                     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('¿Estas seguro?')"]) !!}
