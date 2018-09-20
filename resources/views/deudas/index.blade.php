612a512fce292663113019b10b5441ac5803f75f
MolinaKevin <molinakevinalejandro@gmail.com>
2018-09-20 17:26:08 -0300

    quitado boton de creacion de deuda

--- resources/views/deudas/index.blade.php
+++ resources/views/deudas/index.blade.php
@@ -3,9 +3,6 @@
 @section('content')
     <section class="content-header">
         <h1 class="pull-left">Deudas</h1>
-        <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('deudas.create') !!}">Agregar Nuevo</a>
-        </h1>
     </section>
     <div class="content">
         <div class="clearfix"></div>
