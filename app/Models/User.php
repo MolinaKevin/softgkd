493d88c3f55b1accbd4b6e686ea8295b1d59adca
MolinaKevin <molinakevinalejandro@gmail.com>
2019-06-24 17:05:57 +0200

    Prueba cambio de metodo usuario inactivo

--- app/Models/User.php
+++ app/Models/User.php
@@ -113,9 +113,9 @@ class User extends Authenticatable
 
     public function isInactivo()
     {
-        $retorno = $this->asistencias()->where('created_at',">", Carbon::now()->subMonth()->startOfMonth())->get();
+        $retorno = $this->asistencias()->where('created_at',">", Carbon::now()->subMonth()->startOfMonth())->first();
 
-        return (bool) $retorno->first();
+        return (bool) $retorno;
     }
 
     public function hasFamilia()
