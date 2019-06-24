5c4084fbf49d2fb96ac397f3704ac3bb21aa135d
MolinaKevin <molinakevinalejandro@gmail.com>
2019-06-24 16:50:43 +0200

    Prueba Inactivo

--- app/Models/User.php
+++ app/Models/User.php
@@ -112,7 +112,10 @@ class User extends Authenticatable
 
     public function isInactivo()
     {
-        return (bool) ! $this->asistencias()->where('created_at','>', Carbon::now()->subMonth()->startOfMonth());
+        $retorno  = $this->asistencias()->whereDoesntHave('user.asistencias', function ($q){
+            $q->where('created_at',">", Carbon::now()->subMonth()->startOfMonth());
+        });
+        return (bool) $retorno;
     }
 
     public function hasFamilia()
