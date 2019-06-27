87ae730899ab1fffa3485e8bd11adb94bedf0a72
MolinaKevin <molinakevinalejandro@gmail.com>
2019-06-27 23:12:44 +0200

    Arreglado inactivo para nuevos usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -113,7 +113,7 @@ class User extends Authenticatable
 
     public function isInactivo()
     {
-        if ($this->created_at > Carbon::now()->subMonth()) {
+        if ($this->created_at < Carbon::now()->subMonth()) {
             $retorno = (bool) $this->asistencias()->where('created_at',">", Carbon::now()->subMonth()->startOfMonth())->first();
             return !$retorno;
         }
