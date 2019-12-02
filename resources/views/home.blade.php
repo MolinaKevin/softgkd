6a5efaac2e2533ab99449c3f5fa297360734fa6f
root <root@DESKTOP-9EK2PEI.localdomain>
2019-12-02 01:10:04 +0100

    subiendo cambios para ingresados

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,56 +64,57 @@
                         </div>
                     </div>
                 </div>
-                    <div class="box box-warning col-md-6">
-                        <div class="box-header with-border">
-                            <h3 class="box-title">Deben Ingresar</h3>
+                <div class="box box-danger">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Caja actual</h3>
 
-                            <div class="box-tools pull-right">
-                                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i
-                                            class="fa fa-minus"></i>
-                                </button>
-                                <button type="button" class="btn btn-box-tool" data-widget="remove"><i
-                                            class="fa fa-times"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <!-- /.box-header -->
-                        <div class="box-body">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h1>{{ $ingresables }}</h1>
-                                    <!-- ./chart-responsive -->
-                                </div>
-                                <!-- /.col -->
-                            </div>
-                            <!-- /.row -->
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i
+                                        class="fa fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-box-tool" data-widget="remove"><i
+                                        class="fa fa-times"></i></button>
                         </div>
                     </div>
-                    <div class="box box-danger col-md-6">
-                        <div class="box-header with-border">
-                            <h3 class="box-title">Personas en dispositivo</h3>
-
-                            <div class="box-tools pull-right">
-                                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i
-                                            class="fa fa-minus"></i>
-                                </button>
-                                <button type="button" class="btn btn-box-tool" data-widget="remove"><i
-                                            class="fa fa-times"></i></button>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h1>{{ $ingresables }}</h1>
+                                <!-- ./chart-responsive -->
                             </div>
+                            <!-- /.col -->
                         </div>
-                        <!-- /.box-header -->
-                        <div class="box-body">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h1>{{ $ingresados }}</h1>
-                                    <!-- ./chart-responsive -->
-                                </div>
-                                <!-- /.col -->
-                            </div>
-                            <!-- /.row -->
-                        </div>
-                        <!-- /.box-body -->
+                        <!-- /.row -->
                     </div>
+                    <!-- /.box-body -->
+                </div>
+                <div class="box box-danger">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Caja actual</h3>
+
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i
+                                        class="fa fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-box-tool" data-widget="remove"><i
+                                        class="fa fa-times"></i></button>
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h1>{{ $ingresados }}</h1>
+                                <!-- ./chart-responsive -->
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+
                 <!-- TABLE: Proximas revisaciones -->
                 <div class="box box-warning">
                     <div class="box-header with-border">
