33f88c9641cf5e7e5334c14cc8be928e940769ae
root <root@DESKTOP-9EK2PEI.localdomain>
2019-12-15 21:21:06 +0100

    Boton todos correcto

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/opciones', 'HomeController@opciones')->name('opciones.index');
+
 /**
  *  Resources basicos
  */
