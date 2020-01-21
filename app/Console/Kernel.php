459b5940a350ab8356a2217cb046645ac34774bb
root <root@DESKTOP-9EK2PEI.localdomain>
2020-01-21 23:16:11 +0100

    Scripts opciones

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,8 +27,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('update:planes')->everyThirtyMinutes()->withoutOverlapping();
-        $schedule->command('update:estados')->everyTenMinutes()->withoutOverlapping();
+        $schedule->command('update:planes')->everyThirtyMinutes();
+        $schedule->command('update:estados')->everyTenMinutes();
     }
 
     /**
