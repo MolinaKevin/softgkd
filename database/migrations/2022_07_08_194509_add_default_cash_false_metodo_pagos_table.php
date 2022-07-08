99e37964ee0c2892be82adf5b1ead33be86b4c8a
Kevin Molina <molinakevinalejandro@gmail.com>
2022-07-09 00:49:00 +0200

    repair modal

---
+++ database/migrations/2022_07_08_194509_add_default_cash_false_metodo_pagos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDefaultCashFalseMetodoPagosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('metodo_pagos', function (Blueprint $table) {
+            $table->boolean('cash')->default(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('metodo_pagos', function (Blueprint $table) {
+            $table->boolean('cash')->change();
+        });
+    }
+}
