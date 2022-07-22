323024aea44d1c20b29b9140f4a69972801883b0
Kevin Molina <molinakevinalejandro@gmail.com>
2022-07-22 18:19:11 +0200

    add cajas

--- app/Models/Caja.php
+++ app/Models/Caja.php
@@ -70,6 +70,7 @@ class Caja extends Model
         if (! $this->relationLoaded('user')) {
             $this->load('user');
         }
+        dd($this->relationLoaded('user');
 
         return $this->getRelation('user') ?: $this->defaultUser();
     }
