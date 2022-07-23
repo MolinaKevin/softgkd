5c935bbd60fe62167862c81389cdf36b5aa66f9c
Kevin Molina <molinakevinalejandro@gmail.com>
2022-07-23 23:01:30 +0200

    add tipos pago in metodo pago

--- app/DataTables/MovimientoDataTable.php
+++ app/DataTables/MovimientoDataTable.php
@@ -26,9 +26,8 @@ class MovimientoDataTable extends DataTable
                 return "No definido";
             })
             ->addColumn('metodo', function ($movimiento) {
-                return $movimiento->metodoPago;
-                if (isset($movimiento->metodoPago->name)) {
-                    return $movimiento->metodoPago->name;
+                if (isset($movimiento->metodoPago->title)) {
+                    return $movimiento->metodoPago->title;
                 }
                 return "No definido";
             })
