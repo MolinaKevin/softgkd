e06101212ea7f4afcc0b420e18b5b5e76b500b32
Kevin Molina <molinakevinalejandro@gmail.com>
2022-07-26 19:42:52 +0200

    repair cierres

--- resources/views/cajas/table.blade.php
+++ resources/views/cajas/table.blade.php
@@ -74,7 +74,22 @@
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
-
+<div class="modal modal-success fade" id="modalSuccess" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Exito!</h4>
+            </div>
+            <div class="modal-body" id="bodySuccess">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
 
 @section('scripts')
     @include('layouts.datatables_js')
