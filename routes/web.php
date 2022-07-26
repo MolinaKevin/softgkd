ced15081ee0d0bee7723e9ede93d984802d5715a
Kevin Molina <molinakevinalejandro@gmail.com>
2022-07-26 11:48:53 +0200

    add cierres caja

--- routes/web.php
+++ routes/web.php
@@ -74,6 +74,7 @@ Route::prefix('busqueda')->group(function () {
     Route::get('metodoPagos', 'MetodoPagoController@busqueda')->name('metodoPagos.busqueda');
     Route::get('cajas', 'CajaController@busqueda')->name('cajas.busqueda');
     Route::get('tipoPagos', 'TipoPagoController@busqueda')->name('tipoPagos.busqueda');
+    Route::get('cierres', 'CierreController@busqueda')->name('cierres.busqueda');
 });
 
 Route::prefix('familias/{familia}')->group(function () {
