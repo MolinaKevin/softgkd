585760f92aa2f4e3eeba5804fca730de0d634903
Kevin Molina <molinakevinalejandro@gmail.com>
2022-07-26 13:16:55 +0200

    repair cierres

--- app/Models/Cierre.php
+++ app/Models/Cierre.php
@@ -45,6 +45,13 @@ class Cierre extends Model
         'at' => 'required'
     ];
 
+    /**
+     * Relationships
+     */
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user() {
         return $this->belongsTo(User::class);
     }
