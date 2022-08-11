15f264a1d048cfb7b2563a8d1aa9b208e334b435
Kevin Molina <molinakevinalejandro@gmail.com>
2022-08-11 17:02:48 +0200

    reparar actualizar montos

--- app/Models/Caja.php
+++ app/Models/Caja.php
@@ -82,8 +82,8 @@ class Caja extends Model
             $total = $tipoPago->pivot->monto;
 
             $pagosEfectivo = new Collection();
-            $pagos = $pagosEfectivo->merge($this->pagos()->where('updated_at','>=',$this->cerrado_at)->whereHas('metodoPago', function($query) use($tipoPago){$query->where('tipo_pago_id',$tipoPago->id);})->get());
             dd($tipoPago->id);
+            $pagos = $pagosEfectivo->merge($this->pagos()->where('updated_at','>=',$this->cerrado_at)->whereHas('metodoPago', function($query) use($tipoPago){$query->where('tipo_pago_id',$tipoPago->id);})->get());
             $pagos = $pagosEfectivo->merge($this->movimientos()->where('updated_at','>=',$this->cerrado_at)->whereHas('metodoPago', function($query) use($tipoPago){$query->where('tipo_pago_id',$tipoPago->id);})->get());
 
             foreach($pagos as $pago) {
