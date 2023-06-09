298b15fea6ca912501c66023b2a9f290671988e9
Kevin Molina <molinakevinalejandro@gmail.com>
2023-06-09 19:04:37 +0200

    add tests

--- tests/Feature/UserEstadosTest.php
+++ tests/Feature/UserEstadosTest.php
@@ -96,7 +96,7 @@ class UserEstadosTest extends TestCase
 
 
 		try {
-			$responsep = $this->json('GET', route('api.plans.show', ['id' => $plan->id]));
+			$responsep = $this->json('GET', 'api/plans/' . $plan->id . '/vencimiento');
 			$res = $responsep->json();
 			$vec = $res['data'];
 
