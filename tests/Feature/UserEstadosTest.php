038a40bddc91cf751eb38f543e8a2baa5f9c12fb
Kevin Molina <molinakevinalejandro@gmail.com>
2023-06-09 19:54:06 +0200

    add tests

--- tests/Feature/UserEstadosTest.php
+++ tests/Feature/UserEstadosTest.php
@@ -109,8 +109,6 @@ class UserEstadosTest extends TestCase
 		//dd($response->getContent());
 		
 		$user = User::where('first_name', 'Test')->first();
-		$user = User::all();
-		dd($user);
 
 		try {
 			$responsep = $this->json('GET', 'api/plans/' . $plan->id . '/vencimiento');
