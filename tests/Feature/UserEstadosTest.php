ff5e4f97aee3fbbfbe267641a1833d6c9e23859f
Kevin Molina <molinakevinalejandro@gmail.com>
2023-06-09 18:26:52 +0200

    add tests

--- tests/Feature/UserEstadosTest.php
+++ tests/Feature/UserEstadosTest.php
@@ -2,6 +2,7 @@
 
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Plan;
 use App\Repositories\UserRepository;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -66,5 +67,50 @@ class UserEstadosTest extends TestCase
 		// Comprobar que se redirige a la página correcta
 		$response->assertRedirect(route('users.index')); // O la ruta a la que esperas redirigir
 	}
-	 
+
+	/**
+     * @test create
+     */
+	public function it_asociate_a_plan_to_a_user()
+	{
+		// Arrange: Preparar el usuario que queremos crear
+		$userData = [
+			'first_name' => 'Test',
+			'last_name' => 'User',
+			'email' => 'test@example.com',
+			'password' => 'secret',
+			'password_confirmation' => 'secret',
+			'dni' => 11111111,
+			'sexo' => 'masculino',
+			'fecha_nacimiento' => '2000-01-01',
+			'descuento' => 0
+			// Agrega aquí cualquier otro campo que necesites
+		];
+		$plan = Plan::first();
+
+		// Act: Enviar la solicitud de registro
+		try {
+			//$response = $this->json('POST', route('api.users.store'), $userData);
+
+			$response = $this->ajax('PUT', '/api/users/' . $user->id, [
+				'plans' => [$plan->id],
+			]);
+			$response = $this->json('POST', route('users.store'), $userData);
+		} catch (\Exception $e) {
+			dd($e);
+		}
+		
+
+		// Assert: Verificar que el usuario fue creado correctamente
+		$this->assertDatabaseHas('users', [
+			'first_name' => 'Test',
+			'last_name' => 'User',
+			'email' => 'test@example.com',
+			'estado' => 'Inactivo'
+		]);
+		// Comprobar que el status de respuesta sea correcto (redirección, en este caso)
+		$response->assertStatus(302); // O el código que esperes recibir
+		// Comprobar que se redirige a la página correcta
+		$response->assertRedirect(route('users.index')); // O la ruta a la que esperas redirigir
+	} 
 }
