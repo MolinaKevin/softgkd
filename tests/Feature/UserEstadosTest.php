4926d682cb498e0f67933adb9640709a1040d060
Kevin Molina <molinakevinalejandro@gmail.com>
2023-06-09 18:07:47 +0200

    add tests

--- tests/Feature/UserEstadosTest.php
+++ tests/Feature/UserEstadosTest.php
@@ -39,8 +39,8 @@ class UserEstadosTest extends TestCase
 
 		// Act: Enviar la solicitud de registro
 		try {
-			//$response = $this->json('POST', route('api.users.store'), $userData);
-			$response = $this->json('POST', route('users.store'), $userData);
+			$response = $this->json('POST', route('api.users.store'), $userData);
+			//$response = $this->json('POST', route('users.store'), $userData);
 		} catch (\Exception $e) {
 			dd($e);
 		}
