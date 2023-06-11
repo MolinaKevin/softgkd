4e182e903d23a3ab526aa043b2f696620102947f
Kevin Molina <molinakevinalejandro@gmail.com>
2023-06-11 10:37:13 +0200

    add tests

--- tests/Feature/UserEstadosTest.php
+++ tests/Feature/UserEstadosTest.php
@@ -991,6 +991,7 @@ class UserEstadosTest extends TestCase
 			'caja' => $caja->id,
 			'pago' => $plan->precio / 2,
 		]);
+		dd($response->getContent());
 
 		\Artisan::call('update:estados');
 		\Artisan::call('update:planes');
