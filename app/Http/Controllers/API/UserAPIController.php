084bc35f870cc86791df74032263c0dc27efd2df
Kevin Molina <molinakevinalejandro@gmail.com>
2023-06-16 13:40:39 +0200

    agregar test para desfasaje

--- app/Http/Controllers/API/UserAPIController.php
+++ app/Http/Controllers/API/UserAPIController.php
@@ -111,7 +111,9 @@ class UserAPIController extends AppBaseController
 
         $planes = $user->plans;
 
-        dd($user = $this->userRepository->update($input, $id));
+        $user = $this->userRepository->update($input, $id));
+		var_dump($user);
+		exit();
 
         foreach($planes as $plan) {
             $user->plans()->save($plan);
