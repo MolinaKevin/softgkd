fb0ff18b1af889072b50e3474e8a9d54227509a7
Kevin Molina <molinakevinalejandro@gmail.com>
2023-06-16 15:50:44 +0200

    agregar test para desfasaje

--- app/Models/User.php
+++ app/Models/User.php
@@ -213,6 +213,9 @@ class User extends Authenticatable
 		//if ($pivot->pagado == 0 && $pivot->isVencido()) {
 
 		$last_asistencia = $this->asistencias()->orderBy('created_at', 'desc')->first();
+		if(app()->runningInConsole()) {
+			$pivot->adeudarConDesfasaje($last_asistencia->horario);
+		}
 
 		if ($pivot->pagado == 0 && $pivot->adeudarConDesfasaje($last_asistencia->horario)) {
         	$pivot->adeudar();
