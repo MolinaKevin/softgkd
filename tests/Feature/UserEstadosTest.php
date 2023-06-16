eafeae602a82457ba74035ff2106b98b4f7f6115
Kevin Molina <molinakevinalejandro@gmail.com>
2023-06-16 16:13:12 +0200

    agregar test para desfasaje

--- tests/Feature/UserEstadosTest.php
+++ tests/Feature/UserEstadosTest.php
@@ -1248,8 +1248,8 @@ class UserEstadosTest extends TestCase
 			'vencimiento' => "2023-06-11  00:00:00"
 			// @TODO Revisar vencimiento
 		]);
+
 		$this->assertTrue($plan_user->adeudarConDesfasaje($fecha_asistencia));
-		dd($user->hasDeuda());
 		
 		$this->assertDatabaseHas('users', [
 			'first_name' => 'Test',
